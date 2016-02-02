72dc7f47c319f4ad83b71dd3b5ba26b809779ab7
Arch Nihil <Arch-Nihil@hotmail.com>
2016-02-01 20:23:40 -0500

    Added Mudstone, a type of Sedimentary Stone with corresponding bricks, chunks and dusts. Added it to the worldgen.

--- src/java/com/arch/archgen/blocks/BlocksRegistry.java
+++ src/java/com/arch/archgen/blocks/BlocksRegistry.java
@@ -2,7 +2,9 @@ package com.arch.archgen.blocks;
 
 import com.arch.archgen.blocks.bricks.IgneousBricks;
 import com.arch.archgen.blocks.bricks.MineralBricks;
+import com.arch.archgen.blocks.bricks.SedimentaryBricks;
 import com.arch.archgen.blocks.stone.IgneousStone;
+import com.arch.archgen.blocks.stone.SedimentaryStone;
 import com.arch.archgen.config.Config;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -11,8 +13,10 @@ import net.minecraft.block.Block;
 public class BlocksRegistry {
 	
 	public static Block granite, basalt, gabbro;
+	public static Block mudstone;
 	
 	public static Block graniteBricks, basaltBricks, gabbroBricks;
+	public static Block mudstoneBricks;
 	public static Block quartzBricks, alkaliFeldsparBricks, plagioclaseBricks, hornblendeBricks;
 	
 	public static void init() {
@@ -20,10 +24,15 @@ public class BlocksRegistry {
 		GameRegistry.registerBlock(basalt = new IgneousStone("Basalt", "Plagioclase", "5", 10.0F, 10.0F, 2), BasicItemBlocks.class, "Basalt");
 		GameRegistry.registerBlock(gabbro = new IgneousStone("Gabbro", "Plagioclase", "7", 10.0F, 10.0F, 2), BasicItemBlocks.class, "Gabbro");
 		
+		GameRegistry.registerBlock(mudstone = new SedimentaryStone("Mudstone", "Quartz, Orthoclase, AlkaliFeldspar", "9,5,3", 10.0F, 10.0F, 1), BasicItemBlocks.class, "Mudstone");
+		
 		if (Config.doGenBricks) {
 			GameRegistry.registerBlock(graniteBricks = new IgneousBricks("Granite", 10.0F, 10.0F, 2), BasicItemBlocks.class, "GraniteBricks");
 			GameRegistry.registerBlock(basaltBricks = new IgneousBricks("Basalt", 10.0F, 10.0F, 2), BasicItemBlocks.class, "BasaltBricks");
 			GameRegistry.registerBlock(gabbroBricks = new IgneousBricks("Gabbro", 10.0F, 10.0F, 2), BasicItemBlocks.class, "GabbroBricks");
+			
+			GameRegistry.registerBlock(mudstoneBricks = new SedimentaryBricks("Mudstone", 10.0F, 10.0F, 2), BasicItemBlocks.class, "MudstoneBricks");
+			
 			GameRegistry.registerBlock(quartzBricks = new MineralBricks("Quartz", 10.0F, 10.0F, 2), BasicItemBlocks.class, "QuartzBricks");
 			GameRegistry.registerBlock(alkaliFeldsparBricks = new MineralBricks("AlkaliFeldspar", 10.0F, 10.0F, 2), BasicItemBlocks.class, "AlkaliFeldsparBricks");
 			GameRegistry.registerBlock(plagioclaseBricks = new MineralBricks("Plagioclase", 10.0F, 10.0F, 2), BasicItemBlocks.class, "PlagioclaseBricks");
--- src/java/com/arch/archgen/items/ItemsRegistry.java
+++ src/java/com/arch/archgen/items/ItemsRegistry.java
@@ -3,10 +3,13 @@ package com.arch.archgen.items;
 import com.arch.archgen.config.Config;
 import com.arch.archgen.items.brick.IgneousBrick;
 import com.arch.archgen.items.brick.MineralBrick;
+import com.arch.archgen.items.brick.SedimentaryBrick;
 import com.arch.archgen.items.chunk.IgneousChunk;
 import com.arch.archgen.items.chunk.MineralChunk;
+import com.arch.archgen.items.chunk.SedimentaryChunk;
 import com.arch.archgen.items.dust.IgneousDust;
 import com.arch.archgen.items.dust.MineralDust;
+import com.arch.archgen.items.dust.SedimentaryDust;
 
 import net.minecraft.item.Item;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -14,18 +17,24 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ItemsRegistry {
 
 	public static Item graniteDust, basaltDust, gabbroDust;
+	public static Item mudstoneDust;
 	public static Item quartzDust, alkaliFeldsparDust, plagioclaseDust, hornblendeDust;
 	
 	public static Item graniteChunk, basaltChunk, gabbroChunk;
+	public static Item mudstoneChunk;
 	public static Item quartzChunk, alkaliFeldsparChunk, plagioclaseChunk, hornblendeChunk;
 	
 	public static Item graniteBrick, basaltBrick, gabbroBrick;
+	public static Item mudstoneBrick;
 	public static Item quartzBrick, alkaliFeldsparBrick, plagioclaseBrick, hornblendeBrick;
 	
 	public static void init() {
 		GameRegistry.registerItem(graniteDust = new IgneousDust("Granite"), "GraniteDust");
 		GameRegistry.registerItem(basaltDust = new IgneousDust("Basalt"), "BasaltDust");
 		GameRegistry.registerItem(gabbroDust = new IgneousDust("Gabbro"), "GabbroDust");
+		
+		GameRegistry.registerItem(mudstoneDust = new SedimentaryDust("Mudstone"), "MudstoneDust");
+		
 		GameRegistry.registerItem(quartzDust = new MineralDust("Quartz"), "QuartzDust");
 		GameRegistry.registerItem(alkaliFeldsparDust = new MineralDust("AlkaliFeldspar"), "AlkaliFeldsparDust");
 		GameRegistry.registerItem(plagioclaseDust = new MineralDust("Plagioclase"), "PlagioclaseDust");
@@ -34,6 +43,9 @@ public class ItemsRegistry {
 		GameRegistry.registerItem(graniteChunk = new IgneousChunk("Granite"), "GraniteChunk");
 		GameRegistry.registerItem(basaltChunk = new IgneousChunk("Basalt"), "BasaltChunk");
 		GameRegistry.registerItem(gabbroChunk = new IgneousChunk("Gabbro"), "GabbroChunk");
+		
+		GameRegistry.registerItem(mudstoneChunk = new SedimentaryChunk("Mudstone"), "MudstoneChunk");
+		
 		GameRegistry.registerItem(quartzChunk = new MineralChunk("Quartz"), "QuartzChunk");
 		GameRegistry.registerItem(alkaliFeldsparChunk = new MineralChunk("AlkaliFeldspar"), "AlkaliFeldsparChunk");
 		GameRegistry.registerItem(plagioclaseChunk = new MineralChunk("Plagioclase"), "PlagioclaseChunk");
@@ -43,6 +55,9 @@ public class ItemsRegistry {
 			GameRegistry.registerItem(graniteBrick = new IgneousBrick("Granite"), "GraniteBrick");
 			GameRegistry.registerItem(basaltBrick = new IgneousBrick("Basalt"), "BasaltBrick");
 			GameRegistry.registerItem(gabbroBrick = new IgneousBrick("Gabbro"), "GabbroBrick");
+			
+			GameRegistry.registerItem(mudstoneBrick = new SedimentaryBrick("Mudstone"), "MudstoneBrick");
+			
 			GameRegistry.registerItem(quartzBrick = new MineralBrick("Quartz"), "QuartzBrick");
 			GameRegistry.registerItem(alkaliFeldsparBrick = new MineralBrick("AlkaliFeldspar"), "AlkaliFeldsparBrick");
 			GameRegistry.registerItem(plagioclaseBrick = new MineralBrick("Plagioclase"), "PlagioclaseBrick");
